99fcee4f26db0f15a590e30c21cb56827b36f9be
Kamal Galrani <kamal.galrani@tvarit.com>
2022-03-24 21:37:13 +0100

    fixes emails

--- pkg/services/notifications/notifications_test.go
+++ pkg/services/notifications/notifications_test.go
@@ -184,7 +184,7 @@ func TestSendEmailAsync(t *testing.T) {
 		sentMsg := <-sut.mailQueue
 		assert.Contains(t, sentMsg.Body["text/html"], "body")
 		assert.NotContains(t, sentMsg.Body["text/plain"], "body")
-		assert.Equal(t, "Reset your Grafana password - asd@asd.com", sentMsg.Subject)
+		assert.Equal(t, "Reset your Tvarit Industrial AI password - asd@asd.com", sentMsg.Subject)
 		assert.NotContains(t, sentMsg.Body["text/html"], "Subject")
 		assert.NotContains(t, sentMsg.Body["text/plain"], "Subject")
 	})
