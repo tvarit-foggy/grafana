e3bd67394b21aba57e93ab5519543bf7169afb8f
Priyankg101 <39451634+Priyankg101@users.noreply.github.com>
2023-10-16 12:46:52 +0530

    instance name bug

--- .github/tvarit/deploy_to_dev_16.sh
+++ .github/tvarit/deploy_to_dev_16.sh
@@ -139,8 +139,8 @@ aws ecr get-login-password --region eu-central-1 | docker login --username AWS -
 docker tag grafana/grafana:latest 250373516626.dkr.ecr.eu-central-1.amazonaws.com/lightsailinstance:latest
 docker push 250373516626.dkr.ecr.eu-central-1.amazonaws.com/lightsailinstance:latest
 
-instance_name=grafana-${PREFIX}
-new_instance_name=grafana-${PREFIX}-new
+new_instance_name=grafana-${PREFIX}
+instance_name=grafana-${PREFIX}-new
 
 # Check if previous instance exists
 return_value_instance=$(validate_lightsail_instance $instance_name)
@@ -179,6 +179,6 @@ fi
 
 
 # Open necessary ports on the instance
-aws lightsail open-instance-public-ports --port-info fromPort=3000,toPort=3000,protocol=TCP --instance-name $instance_name
+aws lightsail open-instance-public-ports --port-info fromPort=3000,toPort=3000,protocol=TCP --instance-name $new_instance_name
 
 echo "Deployment completed!"
